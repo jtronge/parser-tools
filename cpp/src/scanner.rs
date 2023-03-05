50cf127120c6e97487658b7e026f33e489135717
Jake Tronge <jtronge3@gmail.com>
2023-03-05 13:45:58 -0500

    Comment out bad scanner code and tests for macro argument parsing

--- cpp/src/scanner.rs
+++ cpp/src/scanner.rs
@@ -19,6 +19,7 @@ impl Scanner {
         }
     }
 
+/*
     /// Check if this token matches a macro name.
     fn find_macro(&self, tok: &Token) -> Option<Rc<Macro>> {
         match tok {
@@ -39,8 +40,10 @@ impl Scanner {
         Ok(())
     }
 
+*/
     /// Scan for more tokens.
     pub fn scan(&mut self) -> Result<()> {
+/*
         if let Some(tok) = self.state.next() {
             let tok = tok?;
             if let Some(mac) = self.find_macro(&tok) {
@@ -50,26 +53,112 @@ impl Scanner {
             }
         }
         Ok(())
+*/
+        Ok(())
     }
 }
 
-fn get_args(ti: impl Iterator<Item=Token>) -> Result<Vec<Vec<Token>>> {
+/// Get the args for a functional macro.
+fn get_args<'a>(ti: impl Iterator<Item=Token> + 'a) -> Result<Vec<Vec<Token>>> {
     let mut args = vec![];
     let mut ti = ti.peekable();
+    // Assume opening parenthesis
+    let _ = ti.next();
     loop {
+        println!("QUI: {:?}!\n", ti.peek());
         let mut arg = vec![];
+        let mut paren_bal = 0;
         loop {
             match ti.peek() {
-                Some(Token::Comma) | Some(Token::RParen) | None => break,
+                Some(Token::LParen) => paren_bal += 1,
+                Some(Token::RParen) => {
+                    if paren_bal == 0 {
+                        break;
+                    } else {
+                        paren_bal -= 1;
+                    }
+                }
+                Some(Token::Comma) => {
+                    if paren_bal == 0 {
+                        break;
+                    }
+                }
+                None => break,
                 _ => (),
             }
-            arg.push(ti.next().unwrap());
+            arg.push(ti.next().unwrap().clone());
         }
         match ti.peek() {
             None => return Err(Error::MissingClosingParenMacroCall),
-            Some(Token::RParen) => break,
+            Some(Token::Comma) => {
+                let _ = ti.next();
+                args.push(arg);
+            }
+            Some(Token::RParen) => {
+                if arg.len() > 0 || args.len() > 0 {
+                    args.push(arg);
+                }
+                let _ = ti.next();
+                break;
+            }
             _ => (),
         }
     }
     Ok(args)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn no_args() {
+        let toks = vec![
+            Token::LParen,
+            Token::RParen,
+        ];
+
+        let args = get_args(toks.iter().map(|tok| tok.clone())).unwrap();
+
+        assert_eq!(args.len(), 0);
+    }
+
+    #[test]
+    fn one_arg() {
+        let toks = vec![
+            Token::LParen,
+            Token::Ident("Ciao, mondo".to_string()),
+            Token::RParen,
+        ];
+
+        let args = get_args(toks.iter().map(|tok| tok.clone())).unwrap();
+
+        assert_eq!(args.len(), 1);
+        assert_eq!(args[0], vec![Token::Ident("Ciao, mondo".to_string())]);
+    }
+
+    #[test]
+    fn some_more_args() {
+        let toks = vec![
+            Token::LParen,
+            Token::Ident("abc".to_string()),
+            Token::Comma,
+            Token::LParen,
+            Token::StringLit("something".to_string()),
+            Token::RParen,
+            Token::RParen,
+        ];
+
+        let args = get_args(toks.iter().map(|tok| tok.clone())).unwrap();
+
+        assert_eq!(args.len(), 2);
+        assert_eq!(args[0], vec![
+            Token::Ident("abc".to_string()),
+        ]);
+        assert_eq!(args[1], vec![
+            Token::LParen,
+            Token::StringLit("something".to_string()),
+            Token::RParen,
+        ]);
+    }
 }
