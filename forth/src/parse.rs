8a8effa5304f7ddb30e6c900410beb7abe03e75e
Jake Tronge <jtronge3@gmail.com>
2023-04-03 20:33:35 -0400

    Add integer test to Forth parse module and code to TokenStream

--- forth/src/parse.rs
+++ forth/src/parse.rs
@@ -1,15 +1,43 @@
+//! Forth parsing module
+#[derive(Debug, PartialEq)]
 pub enum ParseError {}
 
 type ParseResult<T> = std::result::Result<T, ParseError>;
 
-pub enum Token {}
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Ident(String),
+    Integer(i64),
+}
 
-pub struct TokenStream;
+pub struct TokenStream<'a> {
+    code: &'a str,
+}
 
-impl Iterator for TokenStream {
+impl<'a> TokenStream<'a> {
+    pub fn new(code: &'a str) -> TokenStream<'a> {
+        TokenStream {
+            code,
+        }
+    }
+}
+
+impl<'a> Iterator for TokenStream<'a> {
     type Item = ParseResult<Token>;
 
     fn next(&mut self) -> Option<Self::Item> {
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn test_integer() {
+        let mut stream = TokenStream::new("123");
+
+        assert_eq!(stream.next().unwrap(), Ok(Token::Integer(123)));
+        assert_eq!(stream.next(), None);
     }
 }
