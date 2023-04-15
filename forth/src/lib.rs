187b5633d361afa0736de2e58482ffc59f6d5415
Jake Tronge <jtronge3@gmail.com>
2023-04-14 20:16:31 -0400

    Add initial prep-parse step of input

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -14,10 +14,46 @@ pub enum ExecError {
     NotImplemented,
 }
 
+pub enum StackItem {
+    Operation(usize),
+    Integer(i64),
+    Real(f64),
+}
+
 pub type Result<T> = std::result::Result<T, ExecError>;
 
 pub fn execute(code: &str) -> Result<ExecResult> {
     let toks: Vec<Token> = TokenStream::new(code).map(|tok| tok.unwrap()).collect();
+    let mut sym_ids = vec![];
+    let items: Vec<StackItem> = toks
+        .iter()
+        .map(|tok| {
+            match tok {
+                Token::Ident(ref ident) => {
+                    // Find the ID for each symbol
+                    let id = if let Some(id) = sym_ids
+                            .iter()
+                            .position(|other_ident| other_ident == ident) {
+                        id
+                    } else {
+                        let id = sym_ids.len();
+                        sym_ids.push(ident.to_string());
+                        id
+                    };
+                    StackItem::Operation(id)
+                }
+                Token::Number(ref num) => {
+                    if let Ok(i) = i64::from_str(num) {
+                        StackItem::Integer(i)
+                    } else if let Ok(f) = f64::from_str(num) {
+                        StackItem::Real(f)
+                    } else {
+                        panic!("Invalid number: {}", num);
+                    }
+                }
+            }
+        })
+        .collect();
     let mut stack = vec![];
     for tok in toks {
         match tok {
